225ca400d034b02465857b02ecf1d24dc145c61d
Anwar-3108 <syed.sahab9628@gmail.com>
2024-06-28 00:31:42 +0530

    scope

---
+++ Practice-JavaScript/Module2/day2/Block-Scope-and-Global-Scope/scopes.js
@@ -0,0 +1,14 @@
+// var x = 20;
+
+// function foo() {
+//   console.log(x);
+//   var x = 30;
+// }
+
+// foo();
+
+for (var i = 1; i <= 3; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
+}
