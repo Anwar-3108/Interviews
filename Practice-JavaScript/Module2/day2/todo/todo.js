0831dae24f36e89a803d541f14540c3b3158e89b
Anwar-3108 <syed.sahab9628@gmail.com>
2024-06-30 00:39:39 +0530

    updated todo

--- Practice-JavaScript/Module2/day2/todo/todo.js
+++ Practice-JavaScript/Module2/day2/todo/todo.js
@@ -1,46 +1,62 @@
 submit.addEventListener("click", (e) => {
   e.preventDefault();
-  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+  AddTodos();
+});
 
+
+deletebtn.addEventListener("click", (e)=>{
+e.preventDefault()
+  localStorage.removeItem("todos") ;
+  ShowTodos([])
+})
+
+function AddTodos() {
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
   let newTodos = {
     id: Date.now(),
-    titile: title.value,
+    title: title.value,
     desc: desc.value,
   };
 
   todos.push(newTodos);
 
   localStorage.setItem("todos", JSON.stringify(todos));
-  updateTodos(todos);
+  ShowTodos(todos);
 
   title.value = "";
   desc.value = "";
-});
+}
 
-deletebtn.addEventListener("click", () => {
-  localStorage.removeItem("todos");
-  updateTodos([]);
-});
-
-function updateTodos(todos) {
-  todo.innerHTML = todos.map((todoItems) => {
-    return `<div class="card">
-    <h5 class="card-header">${todoItems.titile}</h5>
-    <div class="card-body">
-      <p class="card-text">${todoItems.desc}</p>
+function ShowTodos(todos) {
+  todo.innerHTML = todos.map((items) => {
+    return `
+  
+    <div class="container item-center" >
+    <h3>${items.title}</h3>
+    <p>${items.desc}</p>
+    <button class="btn btn-danger" onclick="deteleTodo(${items.id})" >Delete</button>
+    
+    
     </div>
-    <button class="btn btn-danger" onclick="deleteTodo(${todoItems.id})">Delete</button>
-  </div>`;
+    
+    
+    
+    `;
   });
 }
 
-function deleteTodo(id) {
+function deteleTodo(id) {
+
   let todos = JSON.parse(localStorage.getItem("todos")) || [];
-  todos = todos.filter((todo) => todo.id !== id); // Remove the todo with the specified id
+
+  todos = todos.filter((items) => items.id !== id);
+
   localStorage.setItem("todos", JSON.stringify(todos));
-  updateTodos(todos);
+
+  ShowTodos(todos);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  updateTodos(JSON.parse(localStorage.getItem("todos")||[]));
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+  ShowTodos(todos);
 });
